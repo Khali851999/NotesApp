e5d7f10c33bfc4203803cdd7857a2f25345d3061
Khali851999 <akshatjindal215@gmail.com>
2020-11-14 16:08:40 +0530

    ViewModel added

--- app/src/main/java/com/akshat/notesapp/ViewModel/NotesViewModel.kt
+++ app/src/main/java/com/akshat/notesapp/ViewModel/NotesViewModel.kt
@@ -1,8 +1,19 @@
 package com.akshat.notesapp.ViewModel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.akshat.notesapp.dao.Note
+import com.akshat.notesapp.db.NotesDB
 import com.akshat.notesapp.repository.NotesRepository
 
-class NotesViewModel(private val notesRepository: NotesRepository): ViewModel() {
+class NotesViewModel(application: Application): AndroidViewModel(application) {
 
+    var allNotes: LiveData<ArrayList<Note>>
+
+    init {
+        val dao = NotesDB.getDatabase(application).notesDao()
+        val db = NotesRepository(dao)
+        allNotes = db.allNotes
+    }
 }
